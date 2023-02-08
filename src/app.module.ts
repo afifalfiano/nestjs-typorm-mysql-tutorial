5aebc4597662fc7250e62e12672610073eda11d1
Afif Alfiano <m.afif@cakap.id>
2023-02-08 13:16:14 +0700

    init

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './typeorm/entities/User';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nestjs_mysql_tutorial',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/typeorm/entities/User.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn({type: 'bigint'})
+  id: number;
+
+  @Column({unique: true})
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  createdAt: Date;
+
+  @Column({nullable: true})
+  authStrategy: string;
+
+}
